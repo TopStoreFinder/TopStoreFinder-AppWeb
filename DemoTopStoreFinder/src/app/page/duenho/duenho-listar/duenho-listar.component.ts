3d98f81de43b9ac0fe67ed4a5c9304e3742b649f
Carlos Alberto Quispe Guzman <carloqg45@gmail.com>
2022-09-17 13:44:02 -0500

    solucion de errores de dueño listar y ubucacion listar

--- DemoTopStoreFinder/src/app/page/duenho/duenho-listar/duenho-listar.component.ts
+++ DemoTopStoreFinder/src/app/page/duenho/duenho-listar/duenho-listar.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { MatTableDataSource } from '@angular/material/table';
+import { Duenho } from 'src/app/model/duenho';
+import { DuenhoService } from 'src/app/service/duenho.service';
 
 @Component({
   selector: 'app-duenho-listar',
@@ -6,10 +9,14 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./duenho-listar.component.css']
 })
 export class DuenhoListarComponent implements OnInit {
-
-  constructor() { }
+  datasource: MatTableDataSource<Duenho> = new MatTableDataSource();
+  displayedColumns: string[] = ['id', 'nombre','edad','direccion'];
+  constructor(private d: DuenhoService) { }
 
   ngOnInit(): void {
+    this.d.listar().subscribe(data => {
+      this.datasource = new MatTableDataSource(data);
+    })
   }
 
 }
