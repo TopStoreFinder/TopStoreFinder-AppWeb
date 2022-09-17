a5951ed312ac4787356e4cddbf49a22201c73971
RobertoCaceresRivas <113564581+RobertoCaceresRivas@users.noreply.github.com>
2022-09-17 00:52:26 -0500

    Añadir los listar en page
    Creacion de los components tienda y el listar tienda

--- DemoTopStoreFinder/src/app/app.module.ts
+++ DemoTopStoreFinder/src/app/app.module.ts
@@ -3,10 +3,14 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { TiendaComponent } from './page/tienda/tienda.component';
+import { TiendalistarComponent } from './page/tienda/tiendalistar/tiendalistar.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    TiendaComponent,
+    TiendalistarComponent
   ],
   imports: [
     BrowserModule,
