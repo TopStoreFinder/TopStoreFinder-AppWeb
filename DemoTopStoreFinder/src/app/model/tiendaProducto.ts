69350473fd43de0c9217b841609fda0a1fa4356a
RobertoCaceresRivas <113564581+RobertoCaceresRivas@users.noreply.github.com>
2022-09-17 12:24:07 -0500

    Codificacion del tienda producto.ts

---
+++ DemoTopStoreFinder/src/app/model/tiendaProducto.ts
@@ -0,0 +1,7 @@
+import { Producto } from "./producto";
+import { Tienda } from "./tienda";
+export class TiendaProducto {
+    producto_id: Producto = new Producto();
+    tienda_id: Tienda = new Tienda();
+
+}
