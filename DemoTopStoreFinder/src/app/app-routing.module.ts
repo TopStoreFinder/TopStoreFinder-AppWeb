fa39fdb035440860630be8ab7e84b21774e5d6f6
Carlos Alberto Quispe Guzman <carloqg45@gmail.com>
2022-09-17 12:33:33 -0500

    agregacion de los componentes de todos los listar
    importacion y codificacion de los component de los listar

--- DemoTopStoreFinder/src/app/app-routing.module.ts
+++ DemoTopStoreFinder/src/app/app-routing.module.ts
@@ -1,7 +1,65 @@
+import { TipoPagoComponent } from './page/tipo-pago/tipo-pago.component';
+import { CompradorComponent } from './page/comprador/comprador.component';
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { TipoProductoComponent } from './page/tipo-producto/tipo-producto.component';
+import { StockComponent } from './page/stock/stock.component';
+import { UbicacionComponent } from './page/ubicacion/ubicacion.component';
+import { DuenhoComponent } from './page/duenho/duenho.component';
+import { CompradorUbicacionComponent } from './page/comprador-ubicacion/comprador-ubicacion.component';
+import { ReseniaCalificacionComponent } from './page/resenia-calificacion/resenia-calificacion.component';
+import { ProductoComponent } from './page/producto/producto.component';
+import { TiendaProductoComponent } from './page/tienda-producto/tienda-producto.component';
+import { TiendaComponent } from './page/tienda/tienda.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: 'tipopago', component: TipoPagoComponent, children: [
+
+    ]
+  }, {
+    path: 'reseniacalificacion', component: ReseniaCalificacionComponent, children: [
+
+    ]
+  }, {
+    path: 'tipoproducto', component: TipoProductoComponent, children: [
+
+    ]
+  }, {
+    path: 'stock', component: StockComponent, children: [
+
+    ]
+  }, {
+    path: 'producto', component: ProductoComponent, children: [
+
+    ]
+  }, {
+    path: 'compradorubicacion', component: CompradorUbicacionComponent, children: [
+
+    ]
+  }, {
+    path: 'ubicacion', component: UbicacionComponent, children: [
+
+    ]
+  }, {
+    path: 'tiendaproducto', component: TiendaProductoComponent, children: [
+
+    ]
+  }, {
+    path: 'tienda', component: TiendaComponent, children: [
+
+    ]
+  }, {
+    path: 'duenho', component: DuenhoComponent, children: [
+
+    ]
+  }, {
+    path: 'comprador', component: CompradorComponent, children: [
+
+    ]
+  }
+
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
