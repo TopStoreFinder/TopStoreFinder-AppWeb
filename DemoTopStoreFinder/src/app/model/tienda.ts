06e8b53ae7f0b10142ec1ba6d9965958a19fbd3c
RobertoCaceresRivas <113564581+RobertoCaceresRivas@users.noreply.github.com>
2022-09-17 12:19:49 -0500

    Tienda.ts
    Codigo tienda .ts

---
+++ DemoTopStoreFinder/src/app/model/tienda.ts
@@ -0,0 +1,12 @@
+import { TipoPago } from "./tipoPago";
+import { Duenho } from "./duenho";
+export class Tienda {
+    id: number = 0;
+    nombre: string = "";
+    direccion: string = "";
+    resenha: string = "";
+    calificacion: number = 0;
+    id_duenho: Duenho = new Duenho();
+    id_tipo_pago: TipoPago = new TipoPago();
+
+}
