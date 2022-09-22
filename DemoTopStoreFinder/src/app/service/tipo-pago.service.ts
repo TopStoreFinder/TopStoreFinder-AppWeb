299ff3822dcd6cefccedb559cbdffdd9aa6ba257
Carlos Alberto Quispe Guzman <carloqg45@gmail.com>
2022-09-21 20:14:29 -0500

    Codificacion de los metodos de insertar , set y get

--- DemoTopStoreFinder/src/app/service/tipo-pago.service.ts
+++ DemoTopStoreFinder/src/app/service/tipo-pago.service.ts
@@ -1,18 +1,27 @@
 import { HttpClient } from '@angular/common/http'
 import { TipoPago } from '../model/tipoPago';
 import { Injectable } from '@angular/core';
+import { Subject } from 'rxjs';
 
 @Injectable({
   providedIn: 'root'
 })
 export class TipoPagoService {
   url: string = "http://localhost:5000/tipopagos"
-
+  private listaCambio = new Subject<TipoPago[]>()
   constructor(private http: HttpClient) { }
 
   listar() {
     return this.http.get<TipoPago[]>(this.url);
   }
-
+  insertar(propietario: TipoPago) {
+    return this.http.post(this.url, propietario);
+  }
+  setLista(listaNueva: TipoPago[]) {
+    this.listaCambio.next(listaNueva);
+  }
+  getLista() {
+    return this.listaCambio.asObservable();
+  }
 
 }
