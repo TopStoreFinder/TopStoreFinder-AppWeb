ff06db7f6af53017a93aa3cf6f5476c1b8e51b91
JohnnyReynaga <u201814271@upc.edu.pe>
2022-09-28 00:38:29 -0500

    Actualizacion del compradorubicacion-listar

--- DemoTopStoreFinder/src/app/page/comprador-ubicacion/compradorubicacion-listar/compradorubicacion-listar.component.ts
+++ DemoTopStoreFinder/src/app/page/comprador-ubicacion/compradorubicacion-listar/compradorubicacion-listar.component.ts
@@ -1,3 +1,5 @@
+import { CompradorubicacionDialogoComponent } from './compradorubicacion-dialogo/compradorubicacion-dialogo.component';
+import { MatDialog } from '@angular/material/dialog';
 import { CompradorUbicacion } from './../../../model/compradorUbicacion';
 import { Component, OnInit } from '@angular/core';
 import { MatTableDataSource } from '@angular/material/table'
@@ -10,13 +12,35 @@ import { CompradorUbicacionService } from 'src/app/service/comprador-ubicacion.s
 })
 export class CompradorubicacionListarComponent implements OnInit {
   datasource: MatTableDataSource<CompradorUbicacion> = new MatTableDataSource();
-  displayedColumns: string[] = ['clienteid', 'ubicacionid'];
-  constructor(private p: CompradorUbicacionService) { }
+  displayedColumns: string[] = ['clienteid', 'ubicacionid','acciones','accion2'];
+  private idMayor: number = 0;
+
+  constructor(private p: CompradorUbicacionService, private dialog: MatDialog) { }
 
   ngOnInit(): void {
     this.p.listar().subscribe(a => {
       this.datasource = new MatTableDataSource(a)
     })
+    this.p.getLista().subscribe(data => {
+      this.datasource = new MatTableDataSource(data);
+    });
+    this.p.getConfirmaEliminacion().subscribe(data => {
+      data == true ? this.eliminar(this.idMayor) : false;
+    });
+  }
+
+  confirmar(id: number) {
+    this.idMayor = id;
+    this.dialog.open(CompradorubicacionDialogoComponent);
+  }
+
+  eliminar(id: number) {
+    this.p.eliminar(id).subscribe(() => {
+      this.p.listar().subscribe(data => {
+        this.p.setLista(data);/* se ejecuta la línea 27*/
+      });
+    });
+
   }
 
 }
