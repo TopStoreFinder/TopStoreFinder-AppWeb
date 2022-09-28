f4a7cc93130a72b72df2c71b33fc42cfda5f8a2a
JohnnyReynaga <u201814271@upc.edu.pe>
2022-09-28 00:37:10 -0500

    Creacion del compradorubicacion-buscar

---
+++ DemoTopStoreFinder/src/app/page/comprador-ubicacion/compradorubicacion-buscar/compradorubicacion-buscar.component.ts
@@ -0,0 +1,28 @@
+import { CompradorUbicacion } from './../../../model/compradorUbicacion';
+import { CompradorUbicacionService } from 'src/app/service/comprador-ubicacion.service';
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-compradorubicacion-buscar',
+  templateUrl: './compradorubicacion-buscar.component.html',
+  styleUrls: ['./compradorubicacion-buscar.component.css']
+})
+export class CompradorubicacionBuscarComponent implements OnInit {
+  textoBuscar: string = ""
+  constructor(private compradorubicacionservice: CompradorUbicacionService) { }
+
+  ngOnInit(): void {
+  }
+  buscar(e: any) {
+    let array: CompradorUbicacion[] = [];
+    this.compradorubicacionservice.listar().subscribe(data => {
+      data.forEach((element, index) => {
+        if (element.clienteid.toString().includes(e.target.value)) {
+          array.push(data[index]);
+        }
+      });
+      this.compradorubicacionservice.setLista(array);
+    })
+  }
+
+}
