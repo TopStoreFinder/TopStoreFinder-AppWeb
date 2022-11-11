11e6b34123455646e0c9172cdb04220d024dbfe2
Carlos Alberto Quispe Guzman <carloqg45@gmail.com>
2022-11-11 16:55:15 -0500

    Codificacion de Home

--- DemoTopStoreFinder/src/app/page/home/home/home.component.ts
+++ DemoTopStoreFinder/src/app/page/home/home/home.component.ts
@@ -1,3 +1,5 @@
+import { TipoProductoService } from './../../../service/tipo-producto.service';
+import { CompradorService } from 'src/app/service/comprador.service';
 import { DuenhoService } from 'src/app/service/duenho.service';
 import { TipoPagoService } from 'src/app/service/tipo-pago.service';
 import { Component, OnInit } from '@angular/core';
@@ -12,7 +14,11 @@ export class HomeComponent implements OnInit {
   countTipoPago: number = 0;
   countTienda: number = 0;
   countDuenho: number = 0;
-  constructor(private tipopago: TipoPagoService, private duenho: DuenhoService, private tienda: TiendaService) { }
+  countComprador: number = 0;
+  countTipoProducto: number = 0;
+
+  constructor(private tipopago: TipoPagoService, private duenho: DuenhoService, private tienda: TiendaService,
+     private comprador:CompradorService, private tipoproducto:TipoProductoService) { }
 
   ngOnInit(): void {
     this.tipopago.getCount().subscribe(data => {
@@ -25,6 +31,13 @@ export class HomeComponent implements OnInit {
 
     this.tienda.getCount().subscribe(data => {
       this.countTienda = data.length;
+    });
+    this.comprador.getCount().subscribe(data => {
+      this.countComprador = data.length;
+    });
+
+    this.tipoproducto.getCount().subscribe(data => {
+      this.countTipoProducto = data.length;
     });
 
   }
